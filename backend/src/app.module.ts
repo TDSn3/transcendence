75cf814882f20d2b152277d5e730d4b2a742df23
Waseem Naseeven <waseemnaseeven@MacBook-Pro-de-Waseem.local>
2024-01-23 13:58:41 +0100

    trying db on my mac

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule} from '@nestjs/typeorm';
+
+TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'db',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  entities: [],
+  synchronize: true,
+  autoLoadEntities: true,
+})
 
 @Module({
   imports: [UsersModule],
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { TypeOrmModule} from '@nestjs/typeorm';
+
 
 @Module({
-  TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'db',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-    entities: [],
-    synchronize: true,
-    autoLoadEntities: true,
-  }),
   controllers: [UsersController],
   providers: [UsersService]
 })
