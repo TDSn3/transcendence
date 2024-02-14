c63d213eafff23f7a2fc3b3aff2fe5e38d1d27c7
SolDavid_76 <david.janusz75@gmail.com>
2024-02-14 13:06:18 +0100

    automated front chat

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,12 +1,14 @@
+import Banner from "./Chat/Banner.tsx";
+import Messages from "./Chat/Messages.tsx";
 import InputBar from "./Chat/InputBar.tsx";
-import MessageList from "./Chat/MessageList.tsx";
 import "./Chat/chat.css";
 
 const Chat = () =>
 {
 	return (
 		<div className="page" id="kekw">
-			<MessageList />
+			<Banner channelName="Coucou" />
+			<Messages />
 			<InputBar />
 		</div>
 	);
---
+++ frontend/src/components/Chat/Messages.tsx
@@ -0,0 +1,62 @@
+import "./chat.css";
+
+const test: MessageProps[] = [
+	{
+		username: "djanusz",
+		profilePictureUrl: "https://cdn.intra.42.fr/users/cbaa9bc0d6c69dd4368bedefe259ede1/djanusz.jpg",
+		message: "Hello",
+	},
+	{
+		username: "bob",
+		profilePictureUrl: "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg",
+		message: "BANANAAAA",
+	},
+	{
+		username: "bob",
+		profilePictureUrl: "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg",
+		message: "BANANAAAA",
+	},
+	{
+		username: "bob",
+		profilePictureUrl: "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg",
+		message: "BANANAAAA",
+	},
+	{
+		username: "djanusz",
+		profilePictureUrl: "https://cdn.intra.42.fr/users/cbaa9bc0d6c69dd4368bedefe259ede1/djanusz.jpg",
+		message: "Hmmmmmmmmm",
+	},
+]
+
+interface MessageProps {
+	username: string,
+	profilePictureUrl: string,
+	message: string,
+}
+
+const Message = ({username, profilePictureUrl, message}: MessageProps) => {
+	const userlink: string = "http://localhost:3001/profile=" + {username};
+	return (
+		<li className="message">
+			<a href={userlink}><img className="profilePicture" src={profilePictureUrl} /></a>
+			<div>
+				<a href={userlink} className="username">{username}</a><br />
+				{message}
+			</div>
+		</li>
+	);
+}
+
+const Messages = () => {
+	return (
+		<ul className="messages">
+			{
+				test.map((value: MessageProps) => (
+					<Message username={value.username} profilePictureUrl={value.profilePictureUrl} message={value.message} />
+				))
+			}
+		</ul>
+	);
+}
+
+export default Messages;
