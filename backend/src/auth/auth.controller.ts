e96059ef314cad4781e5ce21c8ced79c69d8f63b
waseemnaseeven <waseem.naseeven@gmail.com>
2024-02-19 13:05:53 +0100

    cookies done

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,16 +1,27 @@
 // auth.controller.ts
-import { Controller, Post, Body, Get, Req, Res, Query, UseGuards, HttpCode, HttpStatus,} from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { Request, Response } from "express";
-import { ApiTags } from "@nestjs/swagger";
-import { SignIn42Dto } from "./dto/sign-in-42.dto";
-import { ConfigService } from "@nestjs/config";
-import { PrismaService } from "../prisma/prisma.service";
-import { JwtService } from "@nestjs/jwt";
-import { BadRequestException } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Req,
+  Res,
+  Query,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Request, Response } from 'express';
+import { ApiTags } from '@nestjs/swagger';
+import { SignIn42Dto } from './dto/sign-in-42.dto';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { BadRequestException } from '@nestjs/common';
 import { SignInResponse42Dto } from './dto/sign-in-response-42.dto.ts';
-import { IntraUserDataDto } from "./dto/intra-user-data.dto";
-
+import { IntraUserDataDto } from './dto/intra-user-data.dto';
+import { JwtGuard } from './guard/jwt.guard';
 
 @Controller('api/auth')
 @ApiTags('auth')
@@ -19,38 +30,48 @@ export class AuthController {
     private authService: AuthService,
     private prisma: PrismaService,
     private jwtService: JwtService,
-    private configService: ConfigService
-) {}
+    private configService: ConfigService,
+  ) {}
 
-    @Get('signin42')
-    async signin42(@Query() signIn42Dto: SignIn42Dto, @Res() res: Response){
-      return this.authService
+  @Get('signin42')
+  async signin42(@Query() signIn42Dto: SignIn42Dto, @Res() res: Response) {
+    return this.authService
       .signin42(signIn42Dto, res)
       .then((user) => {
         res.status(200).json({
           message: 'User successfully signed in',
-          user});
-        }
-      )
-      .catch(() => {
-				res.status(501).json({  message: 'Bruh...' });
-			});
-    }
-
-    @Get('logout')
-    async logout(@Res() res: Response) {
-      try {
-        // res.clearCookie('isLogin',  { sameSite: 'None', secure: true });
-        // console.log('Logout successful');
-        res.status(200).send('Logout successful');
-      } catch (error) {
-        console.error('Error during logout:', error);
-        res.status(500).json({
-          status: 500,
-          error: 'Error during logout' 
+          user,
         });
-      }
-    }
-    
-}
+      })
+      .catch(() => {
+        res.status(501).json({ message: 'Bruh...' });
+      });
+  }
 
+  // @UseGuards(JwtGuard)
+  @Get('logout')
+  async logout(@Res() res: Response) {
+    return this.authService
+      .logout(res)
+      .then(() => {
+        res.status(200).send({ message: 'User successfully logged out' });
+      })
+      .catch(() => {
+        res.status(501).send({ message: 'Logout doesnt work...' });
+      });
+  }
+
+  @Get('checksession')
+  async checksession(@Req() req: Request, @Res() res: Response) {
+    return this.authService
+      .checksession(req)
+      .then(() => {
+        res.status(200).json({
+          message: 'User is logged in',
+        });
+      })
+      .catch(() => {
+        res.status(501).json({ message: 'User is not logged in' });
+      });
+  }
+}
--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,22 +1,26 @@
-import './login.css';
+import React from 'react';
+import './Login.css';
 
-const Login: React.FC = (): JSX.Element => {
-
-  const handleClick = (event: React.FormEvent) => {
+function SignIn42(): JSX.Element {
+  const handleClick = (event: React.FormEvent): void => {
     event.preventDefault();
-    // const redirect_url = import.meta.env.VITE_APP_42API;
     window.location.href = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-84904227fc0a4c9b2bf80053ac3a28805d432b0970e0fa40c44ce8f1cb1f5403&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FsignIn42&response_type=code';
-  }
+  };
 
   return (
-    <div className='42button'>
-      <h3 style={{ marginBottom: '16px', marginLeft: 0 }}>Log in</h3> 
+    <div className="login42">
+      <h3 style={{ marginBottom: '16px', marginLeft: 0 }}>Log in</h3>
       <button
-        className="42buttonsubmit" onClick={handleClick}
-      > login with 42
+        className="buttonsubmit42"
+        onClick={handleClick}
+        type="button"
+      >
+        {' '}
+        login with 42
       </button>
     </div>
   );
-};
+}
 
-export default Login;
+export default SignIn42;
+
