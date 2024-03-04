31ee5651279181233c10ac32ef958fcb5600c222
waseemnaseeven <waseem.naseeven@gmail.com>
2024-03-04 17:24:59 +0100

    App.tsx refacto, 404NotFound and navbar visible

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,41 +7,42 @@ import Chat from './components/Chat';
 import Game from './components/Game/Game';
 import SignIn42 from './components/SignIn42/SignIn42';
 import Logout from './components/Logout';
-import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 
 import useAuth from './contexts/Auth/useAuth';
 
 import ChatRoutes from './components/Chat/ChatRoutes';
 
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
+
 function App() {
   const { isLoggedIn } = useAuth();
-  const isNavbarVisible = ["/login", "/home", "/chat", "/game", "/profile"].includes(window.location.pathname);
 
-  if (!isLoggedIn) {
-    return (
-      <div className="App container">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signIn42" element={<SignIn42 />} />
-          <Route path="/" element={<Navigate to="/login" />}  />
-        </Routes>
-      </div>
-    );
-  }
-  
+  console.log('isLoggedIn === ', isLoggedIn);
+
   return (
     <div className="App container">
-      {isNavbarVisible && <Navbar />}
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:login" element={<Profile />} />
-          <Route path="/chat" element={<Chat />} />
-          {ChatRoutes()}
-          <Route path="/game" element={<Game />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/login" element={<Navigate to="/home" />}/>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+      {
+        !isLoggedIn ? (
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signIn42" element={<SignIn42 />} />
+            <Route path="/" element={<Navigate to="/login" />} />
+          </Routes>
+        ) : (
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/profile/:login" element={<Profile />} />
+              <Route path="/chat" element={<Chat />} />
+              {ChatRoutes()}
+              <Route path="/game" element={<Game />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="*" element={<NotFoundPage />}/>
+            </Routes>
+          </>
+        )
+      }
     </div>
   );
 }
