61287b80ea6d4e1729f6f3d2a015f8904f58f346
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-03-19 16:17:05 +0100

    overlay friendCard ok

--- frontend/src/components/ProfilePicture/ProfilePicture.tsx
+++ frontend/src/components/ProfilePicture/ProfilePicture.tsx
@@ -3,25 +3,42 @@ import './profile-picture.css';
 interface ProfilePictureProps {
   size: string,
   imageUrl: string,
+  margin?: string,
   marginBottom?: boolean,
 }
 
-function ProfilePicture({ size, imageUrl, marginBottom }: ProfilePictureProps) {
+function ProfilePicture({
+  size,
+  imageUrl,
+  margin,
+  marginBottom,
+}: ProfilePictureProps) {
+  const styleWithoutMargin = {
+    width: `${size}`,
+    height: `${size}`,
+    background: `#000000 url(${imageUrl}) no-repeat center/cover`,
+    marginBottom: `${marginBottom ? '16px' : '0px'}`,
+  };
+
+  const styleWithMargin = {
+    width: `${size}`,
+    height: `${size}`,
+    background: `#000000 url(${imageUrl}) no-repeat center/cover`,
+    marginBottom: `${marginBottom ? '16px' : '0px'}`,
+    margin: `${margin}`,
+  };
+
   return (
     <div
       className="profile-picture-style"
-      style={{
-        width: `${size}`,
-        height: `${size}`,
-        background: `#000000 url(${imageUrl}) no-repeat center/cover`,
-        marginBottom: `${marginBottom ? '16px' : '0px'}`,
-      }}
+      style={margin ? styleWithMargin : styleWithoutMargin}
     />
   );
 }
 
 ProfilePicture.defaultProps = {
   marginBottom: false,
+  margin: null,
 };
 
 export default ProfilePicture;
