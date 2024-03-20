b67d4933029b04716f6214cadaec3116c5772a7c
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-03-20 11:54:22 +0100

    change font-family for better render on all OS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './contexts/Auth/AuthContext';
 import { SocketProvider } from './contexts/Socket/SocketContext';
-import 'normalize.css/normalize.css';
 import 'modern-normalize/modern-normalize.css';
 import './var.css';
 import './index.css';
