9a93062a24e3983b5700e9e6d853a3995c00c3ce
bruzii <brucegervais29@gmail.com>
2024-03-25 15:11:38 +0100

    fix height game

--- frontend/src/components/Game/style.ts
+++ frontend/src/components/Game/style.ts
@@ -4,6 +4,7 @@ export const Canvas = styled.canvas`
   display: flex;
   width: 100%;
   height: fit-content;
+  max-height: 600px;
   @media screen and (max-width: 500px) {
     height: 200px;
   }
