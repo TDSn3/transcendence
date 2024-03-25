c29d7813656d8bf2b6cae8edbdc05dbc5c0b41e5
bruzii <brucegervais29@gmail.com>
2024-03-25 14:48:07 +0100

    make responsive game

--- frontend/src/components/Game/PongGame.tsx
+++ frontend/src/components/Game/PongGame.tsx
@@ -1,78 +1,111 @@
-import  { useRef, useEffect } from "react";
-import GameHeader from "./GameHeader";
-import './PongGame.css';
+import { useRef, useEffect } from 'react';
+import GameHeader from './GameHeader';
+import { Canvas } from './style';
 
-
-const PongGame = ({ gameInfo }: { gameInfo: any }) => {
+function PongGame({ gameInfo }: { gameInfo: any }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const drawGame = () => {
     const canvas = canvasRef.current;
     if (canvas) {
-      const context = canvas.getContext("2d");
+      const context = canvas.getContext('2d');
       if (context) {
-        const drawPaddles = ():void => {
+        const drawPaddles = (): void => {
           context.fillStyle = 'black';
           context.fillRect(0, 0, 800, 500);
           context.fillStyle = 'red';
-          context.fillRect(gameInfo.leftPaddle.pos.x, gameInfo.leftPaddle.pos.y, gameInfo.leftPaddle.width, gameInfo.leftPaddle.height)
-          context.fillRect(gameInfo.rightPaddle.pos.x, gameInfo.rightPaddle.pos.y, gameInfo.rightPaddle.width, gameInfo.rightPaddle.height)
+          context.fillRect(
+            gameInfo.leftPaddle.pos.x,
+            gameInfo.leftPaddle.pos.y,
+            gameInfo.leftPaddle.width,
+            gameInfo.leftPaddle.height,
+          );
+          context.fillRect(
+            gameInfo.rightPaddle.pos.x,
+            gameInfo.rightPaddle.pos.y,
+            gameInfo.rightPaddle.width,
+            gameInfo.rightPaddle.height,
+          );
         };
 
-        const drawBall = ():void => {
+        const drawBall = (): void => {
           context.beginPath();
-          context.arc(gameInfo.ball.pos.x, gameInfo.ball.pos.y, 6, 0, 2 * Math.PI);
+          context.arc(
+            gameInfo.ball.pos.x,
+            gameInfo.ball.pos.y,
+            6,
+            0,
+            2 * Math.PI,
+          );
           context.fillStyle = 'red';
           context.fill();
           context.closePath();
         };
 
-        const drawMiddleLine = ():void => {
+        const drawMiddleLine = (): void => {
           const midlleLinewidth = 1;
-          const middleLineX = (gameInfo.width / 2) - (midlleLinewidth / 2);
+          const middleLineX = gameInfo.width / 2 - midlleLinewidth / 2;
           context.fillStyle = 'white';
           context.fillRect(middleLineX, 0, midlleLinewidth, gameInfo.height);
         };
 
-		const drawScore = ():void => {
-			context.fillStyle = 'white';
-			context.font = '20px Arial';
-			context.fillText(`${gameInfo.score[0]}`, gameInfo.width / 2 - 30, 30);
-			context.fillText(`${gameInfo.score[1]}`, gameInfo.width / 2 + 30, 30);
-		}
+        const drawScore = (): void => {
+          context.fillStyle = 'white';
+          context.font = '20px Arial';
+          context.fillText(`${gameInfo.score[0]}`, gameInfo.width / 2 - 30, 30);
+          context.fillText(`${gameInfo.score[1]}`, gameInfo.width / 2 + 30, 30);
+        };
 
-		const drawCountdown = ():void => {
-			context.font = "50px Arial";
-			context.fillStyle = "white";
-			context.textAlign = "center";
-			context.fillText(`${gameInfo.countdown}`, gameInfo.width / 2, gameInfo.height / 2 - 30);
-		}
+        const drawCountdown = (): void => {
+          context.font = '50px Arial';
+          context.fillStyle = 'white';
+          context.textAlign = 'center';
+          context.fillText(
+            `${gameInfo.countdown}`,
+            gameInfo.width / 2,
+            gameInfo.height / 2 - 30,
+          );
+        };
 
         drawPaddles();
         drawMiddleLine();
         drawBall();
-		if (gameInfo.countdown > 0)
-			drawCountdown();
-		drawScore();
+        if (gameInfo.countdown > 0) drawCountdown();
+        drawScore();
       }
     }
   };
 
   useEffect(() => {
-	  if (gameInfo) {
-    	drawGame();
-	  }
+    if (gameInfo) {
+      drawGame();
+    }
   }, [gameInfo]);
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-	{gameInfo && (
-	<>
-	  <GameHeader gameInfo={gameInfo} />
-      <canvas ref={canvasRef} width={800} height={500} className="canvas" />
-	</>
-	)}
-	  </div>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        width: '100%',
+      }}
+    >
+      {gameInfo && (
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+            width: '100%',
+            backgroundColor: 'black',
+          }}
+        >
+          <GameHeader gameInfo={gameInfo} />
+          <Canvas ref={canvasRef} height={500} width={800} />
+        </div>
+      )}
+    </div>
   );
-};
+}
 
 export default PongGame;
---
+++ frontend/src/components/Game/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Canvas = styled.canvas`
+  display: flex;
+  width: 100%;
+  height: fit-content;
+  @media screen and (max-width: 500px) {
+    height: 200px;
+  }
+`;
